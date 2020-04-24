3020c545229e455e8453387ef897143f05adb79f
Meron Brouwer <meron.brouwer@gmail.com>
2020-04-24 10:58:16 +0200

    [enhancement] Initial commit of the existing ShowCase into its own repo

---
+++ src/main/java/nl/han/showcase/buttons/Quit.java
@@ -0,0 +1,24 @@
+package nl.han.showcase.buttons;
+
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import nl.han.showcase.YaegerShowCase;
+import nl.han.yaeger.engine.entities.entity.Location;
+
+public class Quit extends Button {
+
+    private static final String QUIT = "Quit";
+
+    public Quit(YaegerShowCase showCase) {
+        super(new Location(20, 650), QUIT, showCase, -1);
+        setFill(Color.SNOW);
+        setFont(Font.font("palatino", FontWeight.BOLD, 30));
+    }
+
+    @Override
+    public void onMouseButtonPressed(MouseButton button, double x, double y) {
+        showCase.quitGame();
+    }
+}
