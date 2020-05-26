c5c0d49093b91c6a4def6b328144942e55c7f257
Meron Brouwer <meron.brouwer@gmail.com>
2020-05-26 16:07:20 +0200

    [bugfix] Exported the package with the Entities

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module yaeger.showcase {
     requires hanyaeger.api;
 
     exports nl.han.showcase;
+    exports nl.han.showcase.scenes.entitymaps.tilemaps.entities;
 
     opens backgrounds;
     opens entities;
