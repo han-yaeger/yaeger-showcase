486983991cc59e1e52523dae55d7cc5bb5670daf
Abdel Hajou <abdelwhajou@gmail.com>
2021-02-12 19:08:04 +0100

    Made Swordfish use the new SpriteEntity constructor

--- src/main/java/nl/han/showcase/scenes/composing/entities/waterworld/swordfish/SwordfishSprite.java
+++ src/main/java/nl/han/showcase/scenes/composing/entities/waterworld/swordfish/SwordfishSprite.java
@@ -1,12 +1,11 @@
 package nl.han.showcase.scenes.composing.entities.waterworld.swordfish;
 
-import com.github.hanyaeger.api.engine.Size;
 import com.github.hanyaeger.api.engine.entities.entity.Coordinate2D;
 import com.github.hanyaeger.api.engine.entities.entity.sprite.SpriteEntity;
 
 public class SwordfishSprite extends SpriteEntity {
 
     public SwordfishSprite(Coordinate2D location) {
-        super("entities/swordfish.png", location, new Size(225, 81));
+        super("entities/swordfish.png", location); // Original size is 300x108
     }
 }
