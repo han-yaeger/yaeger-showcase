f25108c50a553b191d03b512005543ed1f736142
Meron Brouwer <meron.brouwer@gmail.com>
2021-06-29 11:00:20 +0200

    [enhancement] Implemented volume control

--- src/main/java/nl/han/showcase/scenes/ridethelightning/RideTheLightningScene.java
+++ src/main/java/nl/han/showcase/scenes/ridethelightning/RideTheLightningScene.java
@@ -1,13 +1,23 @@
 package nl.han.showcase.scenes.ridethelightning;
 
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.EntitySpawnerContainer;
 import com.github.hanyaeger.api.TimerContainer;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
 import nl.han.showcase.YaegerShowCase;
 import nl.han.showcase.scenes.ShowCaseScene;
+import nl.han.showcase.scenes.distance.entities.DistanceText;
+import nl.han.showcase.scenes.ridethelightning.entities.VolumeText;
 import nl.han.showcase.scenes.ridethelightning.entities.spawners.RainSpawner;
 import nl.han.showcase.scenes.ridethelightning.timers.LightningTimer;
 
+import java.util.Set;
+
 public class RideTheLightningScene extends ShowCaseScene implements EntitySpawnerContainer, TimerContainer {
+
+    private static final double BACKGROUND_AUDIO_DELTA = 0.01D;
 
     public RideTheLightningScene(final YaegerShowCase showCase) {
         super(showCase);
@@ -28,7 +38,29 @@ public class RideTheLightningScene extends ShowCaseScene implements EntitySpawne
     }
 
     @Override
+    public void setupEntities() {
+        super.setupEntities();
+
+        var volumeText = new VolumeText(new Coordinate2D(getWidth() - 20, 80), this);
+        volumeText.setAnchorPoint(AnchorPoint.TOP_RIGHT);
+        addEntity(volumeText);
+    }
+
+    @Override
     public void setupTimers() {
         addTimer(new LightningTimer(this));
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        super.onPressedKeysChange(pressedKeys);
+
+        if (pressedKeys.contains(KeyCode.M)) {
+            setBackgroundAudioVolume(getBackgroundAudioVolume() + BACKGROUND_AUDIO_DELTA);
+        }
+
+        if (pressedKeys.contains(KeyCode.N)) {
+            setBackgroundAudioVolume(getBackgroundAudioVolume() - BACKGROUND_AUDIO_DELTA);
+        }
     }
 }
